1cd26b814792c9ae468291c85f87a8655fb734f9
achifred <achifred123@gmail.com>
2020-01-31 11:12:46 +0000

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import React, { Component } from 'react';
-import Home from './component/home';
-import Resume from './component/resume';
-import Project from './component/project';
-import Skills from './component/skills';
+import React, { Component } from "react";
+import Home from "./component/home";
+import Resume from "./component/resume";
+import Project from "./component/project";
+import Skills from "./component/skills";
 
-import { Navbar } from './component/navbar';
-
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Navbar } from "./component/navbar";
+import ReactGA from "react-ga";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/resume" component={Resume} exact />
-            <div>
-              <Navbar />
-              <Route path="/project" component={Project} exact />
-              <Route path="/skill" component={Skills} exact />
-            </div>
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
+    initializeAnalytics = () => {
+        ReactGA.initialize("UA-000000-01");
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    };
+
+    render() {
+        this.initializeAnalytics();
+        return (
+            <BrowserRouter>
+                <div>
+                    <Switch>
+                        <Route path="/" component={Home} exact />
+                        <Route path="/resume" component={Resume} exact />
+                        <div>
+                            <Navbar />
+                            <Route path="/project" component={Project} exact />
+                            <Route path="/skill" component={Skills} exact />
+                        </div>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;

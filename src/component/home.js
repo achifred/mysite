bfeef0968dbf9f36fca36b584281b683fb6df86e
achifred <achifred123@gmail.com>
2020-02-03 21:57:27 +0000

    modified data.js

--- src/component/home.js
+++ src/component/home.js
@@ -1,119 +1,119 @@
-import React, { Component } from 'react';
-import { tech, about } from '../constant/data';
-import { Typography, Grid, Paper } from '@material-ui/core';
-import backgroundpic from '../static/undraw_developer_activity_bv83.svg';
+import React, { Component } from "react";
+import { tech, about } from "../constant/data";
+import { Typography, Grid, Paper } from "@material-ui/core";
+import backgroundpic from "../static/htmlcss.svg";
 
-
-import Footer from './footer';
-import { Href } from './href';
+import Footer from "./footer";
+import { Href } from "./href";
 //import  Navbar from './navbar'
 
 export class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      techinfo: tech,
-      aboutinfo: about
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            techinfo: tech,
+            aboutinfo: about
+        };
+    }
 
-  render() {
-    const { aboutinfo } = this.state;
-    return (
-      <Grid container style={styles.homeContainer}>
-        <Grid container style={styles.contentContainer}>
-         
+    render() {
+        const { aboutinfo } = this.state;
+        return (
+            <Grid container style={styles.homeContainer}>
+                <Grid container style={styles.contentContainer}>
+                    <Grid container style={styles.descContainer}>
+                        <Paper style={styles.paper}>
+                            {aboutinfo.map(item => (
+                                <div key={item.title}>
+                                    <Typography
+                                        style={{
+                                            fontSize: 30,
+                                            marginBottom: 10,
+                                            fontWeight: "bold",
+                                            color: "white"
+                                        }}
+                                    >
+                                        {item.title}
+                                    </Typography>
+                                    <Typography
+                                        key={item}
+                                        style={styles.descText}
+                                    >
+                                        {item.slug}
+                                    </Typography>
 
-          <Grid container style={styles.descContainer}>
-            <Paper style={styles.paper}>
-              {aboutinfo.map(item => (
-                <div key={item.title}>
-                  <Typography
-                    style={{
-                      fontSize: 30,
-                      marginBottom: 10,
-                      fontWeight: 'bold',
-                      color: 'white'
-                    }}
-                  >
-                    {item.title}
-                  </Typography>
-                  <Typography key={item} style={styles.descText}>
-                    {item.slug}
-                  </Typography>
+                                    <Typography
+                                        style={{
+                                            fontSize: 16,
+                                            marginBottom: 10,
 
-                  <Typography
-                    style={{
-                      fontSize: 16,
-                      marginBottom: 10,
-                  
-                      color: 'white'
-                    }}
-                  >
-                    {item.description}
-                  </Typography>
-                </div>
-              ))}
+                                            color: "white"
+                                        }}
+                                    >
+                                        {item.description}
+                                    </Typography>
+                                </div>
+                            ))}
 
-              <Href path="/project" name="View Project" />
-              <Href path="/skill" name="View skill" />
-              <Href path="/resume" name="View Resume" />
-            </Paper>
-          </Grid>
-        </Grid>
+                            <Href path="/project" name="View Project" />
+                            <Href path="/skill" name="View skill" />
+                            <Href path="/resume" name="View Resume" />
+                        </Paper>
+                    </Grid>
+                </Grid>
 
-        <Footer />
-      </Grid>
-    );
-  }
+                <Footer />
+            </Grid>
+        );
+    }
 }
 const styles = {
-  homeContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    textAlign: 'center',
-    height: '100vh',
-    width: '100%',
-    background: `linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    homeContainer: {
+        flex: 1,
+        justifyContent: "center",
+        textAlign: "center",
+        height: "100vh",
+        width: "100%",
+        background: `linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
       url(${backgroundpic})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundAttachment: 'fixed'
-  },
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed"
+    },
 
-  avatarContainer: {
-    justifyContent: 'center',
-    marginTop: 60
-  },
-  avatar: {
-    height: 150,
-    width: 150,
-    alignItem: 'center'
-  },
-  descContainer: {
-    justifyContent: 'center',
-    marginTop: 80
-  },
-  paper: {
-    backgroundColor: 'transparent',
-    width: 700,
-    height: 400,
-    margin: 15
-  },
-  descText: {
-    fontFamily: 'Times New Roman',
-    backgroundColor: 'transparent',
-    color: 'white',
-    fontSize: 30,
-    textAlign: 'center',
+    avatarContainer: {
+        justifyContent: "center",
+        marginTop: 60
+    },
+    avatar: {
+        height: 150,
+        width: 150,
+        alignItem: "center"
+    },
+    descContainer: {
+        justifyContent: "center",
+        marginTop: 80
+    },
+    paper: {
+        backgroundColor: "transparent",
+        width: 700,
+        height: 400,
+        margin: 15
+    },
+    descText: {
+        fontFamily: "Times New Roman",
+        backgroundColor: "transparent",
+        color: "white",
+        fontSize: 30,
+        textAlign: "center",
 
-    marginTop: 10
-  },
-  resumeButton: {
-    backgroundColor: '#2398AB',
-    marginTop: 20
-  },
-  link: { textDecoration: 'none', color: 'white' }
+        marginTop: 10
+    },
+    resumeButton: {
+        backgroundColor: "#2398AB",
+        marginTop: 20
+    },
+    link: { textDecoration: "none", color: "white" }
 };
 export default Home;
--- src/constant/data.js
+++ src/constant/data.js
@@ -1,150 +1,162 @@
-import bakery from '../static/baked-berry-cake-1055272.jpg';
-import laughter from '../static/laughter.jpg'
-import folio from '../static/porfolio.jpeg'
-import pot from '../static/pot.jpg'
-import code from '../static/code.jpg'
+import bakery from "../static/baked-berry-cake-1055272.jpg";
+import laughter from "../static/laughter.jpg";
+import folio from "../static/porfolio.jpeg";
+import pot from "../static/pot.jpg";
+import job from "../static/job.svg";
+import cake from "../static/cake.svg";
 export const tech = [
-  {
-    title: 'javascript'
-  },
-  {
-    title: 'Nodejs'
-  },
-  {
-    title: 'jQuery'
-  },
+    {
+        title: "javascript"
+    },
+    {
+        title: "Nodejs"
+    },
+    {
+        title: "jQuery"
+    },
 
-  {
-    title: 'react js'
-  },
-  {
-    title: 'react-native'
-  },
-  {
-    title: 'Php '
-  },
+    {
+        title: "react js"
+    },
+    {
+        title: "react-native"
+    },
+    {
+        title: "Php "
+    },
 
-  {
-    title: 'sql'
-  },
-  {
-    title: 'mysql'
-  },
-  {
-    title: 'postgresql'
-  }
+    {
+        title: "sql"
+    },
+    {
+        title: "mysql"
+    },
+    {
+        title: "postgresql"
+    }
 ];
 
 export const about = [
-  {
-    title: 'Hi my name is Fred Achi.',
-    slug: "I build things for the web.",
-    description:
-      ' I am web Developer based in Ghana. I develop websites, web applications  and mobile application. I believe nothing is impossible to achieve if you put in your all. '
-  }
+    {
+        title: "Hi my name is Fred Achi.",
+        slug: "I build things for the web.",
+        description:
+            " I am web Developer based in Ghana. I develop websites, web applications  and mobile application. I believe nothing is impossible to achieve if you put in your all. "
+    }
 ];
 export const education = [
-  {
-    name: ' Kwame Nkrumah University of Science and Technology',
-    programe: 'BSc Computer Engineering',
-    year: 'June, 2018'
-  }
+    {
+        name: " Kwame Nkrumah University of Science and Technology",
+        programe: "BSc Computer Engineering",
+        year: "June, 2018"
+    }
 ];
 
 export const project = [
-  {
-    title: 'Achif Dev Jobs',
-    description: 'A job board for developers ',
-    link: 'https://github.com/achifred/remotejob and https://github.com/achifred/remotejobclient',
-    site: 'https://achifjobs.netlify.com/',
+    {
+        title: "Achif Dev Jobs",
+        description: "A job board for developers ",
+        link:
+            "https://github.com/achifred/remotejob and https://github.com/achifred/remotejobclient",
+        site: "https://achifjobs.netlify.com/",
 
-    picture: code
-  },
-  {
-    title: 'Developer jokeroom',
-    description: 'A live chart web application built with nodejs, express, socket.io and react ',
-    link: 'https://github.com/achifred/jokeroomserver',
-    site: 'https://jokeroom.netlify.com/',
+        picture: job
+    },
+    {
+        title: "Rene Cakes",
+        description: "A Business website ",
+        link: "https://github.com/achifred/renecakes",
+        site: "https://renecakes.herokuapp.com/",
 
-    picture: laughter
-  },
-  {
-    title: 'Sample  Business Landing page',
-    description: 'a sample Business landing page built with php, HTML5, CSS3 and javascript. Use https://amenuve.000webhostapp.com/admin username:admin password:admin to access the dashboard',
-    link: 'https://github.com/achifred/amenuveve',
-    site:'https://amenuve.000webhostapp.com/',
-    picture: pot
-  },
-  {
-    title: 'Sample Html Landing Page',
-    description: ' A landing page built with HTML5 & CSS3 ',
-    link: 'https://github.com/achifred/bakery',
-    site: 'https://bake.netlify.com/',
-    picture: bakery
-  },
+        picture: cake
+    },
+    {
+        title: "Developer jokeroom",
+        description:
+            "A live chart web application built with nodejs, express, socket.io and react ",
+        link: "https://github.com/achifred/jokeroomserver",
+        site: "https://jokeroom.netlify.com/",
 
-  {
-    title: 'Portfolio',
-    description: 'A personal webiste built with react.js ',
-    link: 'https://github.com/achifred/mysite',
-    picture: folio
-  }
+        picture: laughter
+    },
+    {
+        title: "Sample  Business Landing page",
+        description:
+            "a sample Business landing page built with php, HTML5, CSS3 and javascript. Use https://amenuve.000webhostapp.com/admin username:admin password:admin to access the dashboard",
+        link: "https://github.com/achifred/amenuveve",
+        site: "https://amenuve.000webhostapp.com/",
+        picture: pot
+    },
+    {
+        title: "Sample Html Landing Page",
+        description: " A landing page built with HTML5 & CSS3 ",
+        link: "https://github.com/achifred/bakery",
+        site: "https://bake.netlify.com/",
+        picture: bakery
+    },
+
+    {
+        title: "Portfolio",
+        description: "A personal webiste built with react.js ",
+        link: "https://github.com/achifred/mysite",
+        picture: folio
+    }
 ];
 
 export const skills = [
-  {
-    title: 'Front-end Development',
-    techstack: 'JavaScript, React.js ,  HTML5, CSS3 '
-  },
-  {
-    title: 'Backend Development',
-    techstack: ' NodeJs, Php, SQL, MySQL, PostgreSQL'
-  },
-  {
-    title: 'Mobile Application Development ',
-    techstack: 'React-native'
-  },
-  {
-    title: 'CMS',
-    techstack: 'Wordpress'
-  }
+    {
+        title: "Front-end Development",
+        techstack: "JavaScript, React.js ,  HTML5, CSS3 "
+    },
+    {
+        title: "Backend Development",
+        techstack: " NodeJs, Expressjs, Php, SQL, MySQL, PostgreSQL"
+    },
+    {
+        title: "Mobile Application Development ",
+        techstack: "React-native"
+    },
+    {
+        title: "CMS",
+        techstack: "Wordpress"
+    }
 ];
 
 export const workexp = [
-  {
-    company: 'Kofi Annan Center of Excellence in ICT, Accra',
-    role: 'Web Application Developer',
-    date: ' september 2018 - August 2019',
-    project: [
-      {
-        role:
-          'Develop an advertisement website for advertising center courses using PHP'
-      },
-      {
-        role:
-          "Developed the frontend for a ledger system. An application to manage center's financial transactions."
-      }
-    ]
-  },
-  {
-    company: 'Appatech Research Ghana, Kumasi',
-    role: 'Web and Mobile Application Developer(intern)',
-    date: ' June 2017- August 2017',
-    project: [
-      {
-        role:
-          'Design and develop a mobile application version of an existing Enterprise Resource Planning '
-      }
-    ]
-  }
+    {
+        company: "Kofi Annan Center of Excellence in ICT, Accra",
+        role: "Web Application Developer",
+        date: " september 2018 - August 2019",
+        project: [
+            {
+                role:
+                    "Develop an advertisement website for advertising center courses using PHP"
+            },
+            {
+                role:
+                    "Developed the frontend for a ledger system. An application to manage center's financial transactions."
+            }
+        ]
+    },
+    {
+        company: "Appatech Research Ghana, Kumasi",
+        role: "Web and Mobile Application Developer(intern)",
+        date: " June 2017- August 2017",
+        project: [
+            {
+                role:
+                    "Design and develop a mobile application version of an existing Enterprise Resource Planning "
+            }
+        ]
+    }
 ];
 
 export const contact = [
-  {
-    name: 'Fred Achi',
+    {
+        name: "Fred Achi",
 
-    country: 'Accra, Ghana',
-    email: 'achifred123@gmail.com',
-    Phone: '+233548480707'
-  }
+        country: "Accra, Ghana",
+        email: "achifred123@gmail.com",
+        Phone: "+233548480707"
+    }
 ];
